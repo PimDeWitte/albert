07fca48edb8f1550d3892c21d68961bc1a671b95
Pim de Witte <pim@medal.tv>
2025-07-24 04:25:22 -0400

    Run commit

---
+++ docs/latest_run/MyTheory_p_1_0/code/theory_instance.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Exact theory instance used in this run"""
+
+# This file shows how the theory was instantiated for this run
+
+from theory_template_theory import MyTheory
+
+# Instantiation with exact parameters
+theory = MyTheory(some_parameter=1.0)
+
+# Theory name: MyTheory (p=1.0)
+# Category: base
